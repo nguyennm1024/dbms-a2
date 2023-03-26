a60626a5b9a1679d69be16204158cd8741433b75
nguyennm <you@example.com>
2023-03-25 20:18:05 -0400

    highlight keywords

--- metadata_search.cc
+++ metadata_search.cc
@@ -5,8 +5,17 @@
 #include <algorithm>
 #include <sstream>
 #include <time.h>
+#include <regex>
 
 using namespace std;
+
+std::string highlight(const std::string& input, const std::string& pattern, const std::string& replacement) {
+    std::string openingTag = "==";
+    std::string closingTag = "==";
+    std::regex regexPattern(pattern);
+    std::string highlighted = std::regex_replace(input, regexPattern, openingTag + replacement + closingTag);
+    return highlighted;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -14,10 +23,12 @@ int main(int argc, char const *argv[])
     std::vector<string> and_terms;
     std::vector<string> or_terms;
     for(int i = 3; i < argc; i++)
-        if (argv[i][0] == '+')
+        if (argv[i][0] == '+') {
             and_terms.push_back(string(argv[i]+1));
+            // cout << string(argv[i]) << endl;
+        }
         else {
-            cout << string(argv[i]) << endl;
+            // cout << string(argv[i]) << endl;
             or_terms.push_back(string(argv[i]));
         }
         // terms.push_back(string(argv[i]));
@@ -36,7 +47,7 @@ int main(int argc, char const *argv[])
     enquire.set_query(or_query);
     Xapian::MSet matches = enquire.get_mset(0, stoi(argv[2]));
     end = clock();
-    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    double time_taken_or = double(end - start) / double(CLOCKS_PER_SEC);
     cout << "Or query" << endl;
     std::cout << matches.size() << std::endl;
     for(Xapian::MSetIterator match = matches.begin(); 
@@ -46,10 +57,12 @@ int main(int argc, char const *argv[])
         Xapian::Document doc = match.get_document();
     
         std::string value0 = doc.get_value(0);
+        for (int i = 0; i < or_terms.size(); i++) {
+            value0 = highlight(value0, or_terms[i], or_terms[i]);
+        }
         cout << value0 << endl;
         cout << endl;
     }
-    cout << "Time taken by or query is : " << fixed << time_taken <<endl;
 
     cout << "====================" << endl;
     cout << "And query" << endl;
@@ -60,12 +73,11 @@ int main(int argc, char const *argv[])
         and_terms.begin(),
         and_terms.end()
     );
-
+    // Xapian::Enquire enquire2(db);
     enquire.set_query(and_query);
     matches = enquire.get_mset(0, stoi(argv[2]));
     end = clock();
-    time_taken = double(end - start) / double(CLOCKS_PER_SEC);
-    cout << "Time taken by and query is : " << fixed << time_taken;
+    double time_taken_and = double(end - start) / double(CLOCKS_PER_SEC);
     std::cout << matches.size() << std::endl;
     for(Xapian::MSetIterator match = matches.begin(); 
     match != matches.end();
@@ -74,9 +86,15 @@ int main(int argc, char const *argv[])
         Xapian::Document doc = match.get_document();
     
         std::string value0 = doc.get_value(0);
+        for (int i = 0; i < and_terms.size(); i++) {
+            value0 = highlight(value0, and_terms[i], and_terms[i]);
+        }
         cout << value0 << endl;
         cout << endl;
-    }
+        }
 
+    cout << "Time taken by or query is : " << fixed << time_taken_or <<endl;
+    cout << "Time taken by and query is : " << fixed << time_taken_and <<endl;
+    
     return 0;
 }
